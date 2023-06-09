95dbe95144189ae120ed30f5b7f05a7dce380e57
Sebastian Claros <sclaros@gmail.com>
2023-06-09 19:45:14 -0300

    agregue que setee el user para debug mode

--- scripts/automation/createScratch.ps1
+++ scripts/automation/createScratch.ps1
@@ -9,13 +9,15 @@ if ( $args[0] -AND $args[1] ) {
     git branch $userstory
     git checkout $userstory
     sf org create scratch -y $dias -a $userstory -d -w 10 --username $email --definition-file $definitionFile
-    sfdx force:user:password:generate
-    sfdx force:source:push
-    sfdx force:user:permset:assign -n dreamhouse
-    sf data import tree --plan .\data\sample-data-plan.json
+    sfdx force:user:password:generate -u $userstory
+    sfdx force:source:push -u $userstory
+    sfdx force:user:permset:assign -n dreamhouse -u $userstory
+    sf data import tree --plan .\data\sample-data-plan.json -u $userstory
     sfdx force:user:display
+    sfdx force:apex:execute -u $userstory -f ..\apex\debugMode.apex
     if ( $omni ) {
-        echo y | sf package install --package 04t5c000000o7RXAAY -w 1000
+        write-host("Instalar Omnistudio puede tardar mucho tiempo, revise el email")
+        echo y | sf package install -u $userstory --package 04t5c000000o7RXAAY
     }
 } else {
     write-host("createScratch story-id story-subject days isOmni.
