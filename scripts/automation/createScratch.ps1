33e9ade6c42c71f50ee66cf0db4168c2e060f1c6
Sebastian Claros <sclaros@gmail.com>
2023-06-13 17:10:40 -0300

    pequeño update script

--- scripts/automation/createScratch.ps1
+++ scripts/automation/createScratch.ps1
@@ -13,12 +13,16 @@ if ( $args[0] -AND $args[1] ) {
     sf project deploy start --target-org $userstory
     sf org assign permset --name dreamhouse --target-org $userstory
     sf data import tree --plan .\data\sample-data-plan.json -u $userstory
-    sfdx org display user
     sf apex run --target-org $userstory -f .\scripts\apex\debugMode.apex
     if ( $args[3] ) {
+        write-host("Por favor active ContactsToMultipleAccounts desde Configuracion > Cuentas > Configuracion de Cuentas ")
+        write-host("https://test.salesforce.com/lightning/setup/AccountSettings/home ")
+        write-host("Modificar y seleccionar Permitir a los usuarios relacionar un contacto con cuentas múltiples y guardar!")
+        
         write-host("Instalar Omnistudio puede tardar mucho tiempo, revise el email")
         echo y | sf package install --target-org $userstory --package 04t5c000000o7RXAAY
     }
+    sfdx org display user
 } else {
     write-host("createScratch story-id story-subject days isOmni.
     ejemplo createScratch BSCCC22 'Cambiar Funcionalidad' 30 Yes")
