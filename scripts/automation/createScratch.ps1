67ee424eedb6278dee1e7c66b6209469127c0ab1
Sebastian Claros <sclaros@gmail.com>
2023-06-13 17:30:15 -0300

    Ej5 (#4)
    * agregue que setee el user para debug mode
    
    * cambio a sf
    
    * resultados ejercicio 1
    
    * rename de archivos
    
    * pequeño update script
    
    * cambio de branch

--- scripts/automation/createScratch.ps1
+++ scripts/automation/createScratch.ps1
@@ -6,19 +6,22 @@ $email =  $userstory + '@teco.com.ar'
 
 if ( $args[0] -AND $args[1] ) {
     #    sf org list --clean --no-prompt
-    #git branch $userstory
-    #git checkout $userstory
+    git branch $userstory
+    git checkout $userstory
     sf org create scratch -y $dias --description $description --name $userstory  -a $userstory -d -w 10 --username $email --definition-file $definitionFile
     sf org generate password --target-org $userstory
     sf project deploy start --target-org $userstory
     sf org assign permset --name dreamhouse --target-org $userstory
     sf data import tree --plan .\data\sample-data-plan.json -u $userstory
-    sfdx org display user
     sf apex run --target-org $userstory -f .\scripts\apex\debugMode.apex
     if ( $args[3] ) {
+        write-host("Por favor active ContactsToMultipleAccounts desde Configuracion > Cuentas > Configuracion de Cuentas ")
+        write-host("https://test.salesforce.com/lightning/setup/AccountSettings/home ")
+        write-host("Modificar y seleccionar Permitir a los usuarios relacionar un contacto con cuentas múltiples y guardar!")
         write-host("Instalar Omnistudio puede tardar mucho tiempo, revise el email")
         echo y | sf package install --target-org $userstory --package 04t5c000000o7RXAAY
     }
+    sfdx org display user
 } else {
     write-host("createScratch story-id story-subject days isOmni.
     ejemplo createScratch BSCCC22 'Cambiar Funcionalidad' 30 Yes")
